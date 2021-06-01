c529c23cc4d14003024795406d9708c1c3a0f5d7
Prateek937 <prateeksanodiya937.com>
2021-06-01 22:23:05 +0530

    using variables from different module

--- terrafom/mym/dev.tf
+++ terrafom/mym/dev.tf
@@ -4,4 +4,7 @@ module "myDevModule" {
     source = "./modules/ec2"
     # have to define own value if is not set in the variables file
     mytype = "t2.micro"
+
+    # for using the  varibale from different module variable file.
+    mytype = "${module.myvpc.cidr}"
 }
